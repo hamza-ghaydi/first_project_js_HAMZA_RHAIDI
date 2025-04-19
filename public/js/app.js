45abfd5baa56585f8e4db390f8ed0b03a12100f8
hamzashiva <hamzaghaydi01@gmail.com>
2025-04-19 01:15:11 +0100

    sign up fix + function valid name

--- public/js/app.js
+++ public/js/app.js
@@ -14,82 +14,80 @@ class Users {
 console.log(data_users);
 
 
-// data_users.push(user)
-function sign_up() {
-    let name = prompt("Enter your Name").toLocaleLowerCase().trim();
-    let email = prompt("Enter valid Email").trim();
-    let age = prompt("how old are you?").toLocaleLowerCase().trim()
-    let password = prompt("Enter Password").trim();
-    let confirm_password = prompt("Confirm your Password").trim();
+// validname function
+function validName(name) {
+    if (name.length < 5){
+        console.log("name is too short, you need at least 5 charachters"); 
+        return false
+    }
+    if (/\d/.test(name)){
+        console.log("remove numbers from name")
+        return false
+    }
+    if (/[@#\-+*/]/.test(name)) {
+            console.log("remove special characters");
+            
+        return false
+    }
+    return true
+}
 
-    if (password !== confirm_password) {
-        alert("password inccorect")
+
+function sign_up() {
+    let name = prompt("Enter your Name").toLocaleLowerCase().trim()
+    if (!validName(name)) {
+        alert("invalid name, the name should > 5 character & no number and no symbol")
         return
+    }
+    let email = prompt("Enter valid Email").trim().toLocaleLowerCase()
+    if (!isValidEmail(email)) {
+        alert("Invalid email. Make sure it's correct and not already used.");
+        return;
+    }
+    let age = parseInt(prompt("how old are you?").trim())
+    if (!isValidAge(age)) {
+        alert("Invalid age. Must be a number with less than 3 digits.");
+        return;
+    }
+    let password = prompt("Enter Password").trim()
+    if (!isValidPassword(password)) {
+        alert("Invalid password. Must be 7+ characters with one special symbol (@, #, -, +, *, /)");
+        return;
+    }
+    let confirm_password = prompt("Confirm your Password").trim()
+    if (password !== confirm_password) {
+        alert("password inccorect, try again")
+        sign_up()
     }
 
     let newuser = new Users(name, email, age, password)
     data_users.push(newuser)
+    alert("welcome to bank lfo9ara")
 }
 
-function login(username) {
-    let email = prompt("Enter your email").toLocaleLowerCase().trim();
-    let password = prompt("enter your password");
-    for (let index = 0; index < data_users.length; index++) {
-        if (email === data_users[index] && password === data_users[index]) {
-            return alert("Login success! Welcome to bank lfo9ara")
-        }
+let user_function = ""
+while (user_function !== "4") {
+    user_function = prompt("hello, welcome to bank lfo9ara \nsetting: \n1 - sign up \n2 - login \n3 - change password \n4 - exit")
+    if (user_function === null) {
+        continue
     }
-    alert("login failed please register")
+    switch (user_function) {
+        case "1":
+            sign_up();
+            break;
+
+        case "2":
+            login();
+            break;
+
+        case "3":
+            change_password()
+            break;
+
+        default:
+            alert("enter the information")
+            break;
+    }
+
+    console.table(data_users);
 }
-
-let user_function = prompt("hello, welcome to bank lfo9ara \nsetting: \n1 - sign up \n2 - login \n3 - withdraw \n4 - Deposit \n5 - exit")
-// while (user_function) {
-
-//     if (user_function === "1") {
-//         sign_up();
-//     } else if (user_function === "2") {
-//         login()
-//     }
-    
-// }
-
-
-
-
-
-
-// function sign_up() {
-//     let name = prompt("Enter your Name").toLocaleLowerCase().trim();
-
-//     let email = prompt("Enter valid Email").trim();
-//     let age = prompt("how old are you?").toLocaleLowerCase().trim()
-//     let password = prompt("Enter Password").toLocaleLowerCase().trim();
-//     let confirm_password = prompt("Confirm your Password").toLocaleLowerCase().trim();
-
-//     if (password !== confirm_password) {
-//         alert("password inccorect")
-//         return
-//     }
-
-//     let newuser = new Users(name, email, age, password)
-//     data_users.push(newuser)
-// }
-
-// function login(username) {
-//     let email = prompt("Enter your Name").toLocaleLowerCase().trim();
-//     let password = prompt("enter your password");
-//     for (let index = 0; index < data_users.length; index++) {
-//         if (email === data_users[index] && password === data_users[index]) {
-//             return alert("Login success! Welcome to bank lfo9ara")
-//         }
-//     }
-//     alert("login failed please register")
-// }
-
-
-console.table(data_users);
-
-
-
-
-
